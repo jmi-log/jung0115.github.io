2f4cb4df0120f596fb4a5707f155dffa45f2bd83
jung0115 <jungim7490@gmail.com>
2023-04-30 02:46:14 +0900

    issue #02 feature: 메인 페이지 Header, Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import './styles/App.css';
+import Header from "./pages/home/Header";
+import Home from "./pages/home/Home";
+import Footer from "./pages/home/Footer";
+import Yolo from "./pages/yolo/Yolo";
 
 function App() {
   return (
-    <div className="App">
-      JungMi's Blog
+    <div style={{ margin: "6vh 10vw" }}>
+      <Router>
+        <Header />
+        
+        <Contents>
+          <Routes>
+            <Route path="/yolo" element={<Yolo />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Contents>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
+
+const Contents = styled.div`
+  height: auto;
+  margin: 4vh 5vw;
+`;
 
 export default App;
