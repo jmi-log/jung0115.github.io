9f2c369fc38289f042ea9432579521b59aa89549
jung0115 <jungim7490@gmail.com>
2023-12-19 22:04:23 +0900

    [FEAT] #14 - posts 탭에 게시물 리스트 배치하고 세부 페이지 이동

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,7 +7,7 @@ import organizationIcon from "../../icons/ic_info_organization.png";
 import emailIcon from "../../icons/ic_info_email.png";
 import githubIcon from "../../icons/ic_info_github.png";
 import MoreContentsBtn from "../../components/MoreContentsBtn";
-import PostItem from "../../components/PostItem";
+import PostItemForHome from "../../components/PostItemForHome";
 import Footer from "../../components/Footer";
 
 const Home = ({ onSelectJmi, onSelectPosts }) => {
@@ -53,8 +53,8 @@ const Home = ({ onSelectJmi, onSelectPosts }) => {
           </InfoContent>
 
           <MoreInfoBtn onClick={onSelectJmi}>
-              <MoreContentsBtn
-                text={"더 자세한 소개"}/>
+            <MoreContentsBtn
+              text={"더 자세한 소개"}/>
           </MoreInfoBtn>
 
         </InfoContainer>
@@ -63,7 +63,7 @@ const Home = ({ onSelectJmi, onSelectPosts }) => {
         <NewPostContainer>
           <NewPostHeader>
             <NewPostTitle>최근 글</NewPostTitle>
-            <MorePostBtn  onClick={onSelectPosts}>
+            <MorePostBtn onClick={onSelectPosts}>
               <MoreContentsBtn text={"더보기"}/>
             </MorePostBtn>
           </NewPostHeader>
@@ -71,7 +71,7 @@ const Home = ({ onSelectJmi, onSelectPosts }) => {
           <NewPostHr/>
 
           <PostList>
-            <PostItem
+            <PostItemForHome
               title={"[Android Studio] Paging"}
               contents={"Android Developer 사이트를 구경하다가 Paging이라는 걸 발견했다. Recyclerview에서 데이터를 페이지로 끊어서 가져올 수 있는 것 같다. 표시해야 할 데이터가 많아서 스크롤이 작아지고 화면이 길어지면 그 데이터를 다 불러올 동안 로딩 시간이 너무 많이 걸리게 된다. 이전에 참여한 앱 프로젝트에서 이런 경우가 있었는데 당시에는 다른 기능을 빨리 마무리해야 해서 해결을 못하고 넘어갔지만 이 방법을 적용하면 될 것 같다.\n\n\n\n\n지금 졸업작품으로 만들고 있는 앱에도 이게 도움이 될 것 같다. 해당 부분에 적용해봐야겠다."}
               date={"2023.09.01 "}
@@ -79,7 +79,7 @@ const Home = ({ onSelectJmi, onSelectPosts }) => {
               viewCount={29}
               image={"https://github.com/jung0115/jung0115/assets/76805879/686139a2-c6fd-46c8-a3b2-657f4e3f3373"}/>
             
-            <PostItem
+            <PostItemForHome
               title={"[Android Studio] Paging"}
               contents={"Android Developer 사이트를 구경하다가 Paging이라는 걸 발견했다. Recyclerview에서 데이터를 페이지로 끊어서 가져올 수 있는 것 같다. 표시해야 할 데이터가 많아서 스크롤이 작아지고 화면이 길어지면 그 데이터를 다 불러올 동안 로딩 시간이 너무 많이 걸리게 된다. 이전에 참여한 앱 프로젝트에서 이런 경우가 있었는데 당시에는 다른 기능을 빨리 마무리해야 해서 해결을 못하고 넘어갔지만 이 방법을 적용하면 될 것 같다.\n\n\n\n\n지금 졸업작품으로 만들고 있는 앱에도 이게 도움이 될 것 같다. 해당 부분에 적용해봐야겠다."}
               date={"2023.09.01 "}
---
+++ src/pages/posts/PostDetail.js
@@ -0,0 +1,149 @@
+// 게시물 세부
+import styled from "styled-components";
+import palette from "../../styles/colorPalette";
+import Footer from "../../components/Footer";
+
+import shareIcon from "../../icons/ic_share.png";
+
+const PostDetail = () => {
+  return (
+    <ContentFooter>
+      <Container>
+        {/* 게시글 본문 */}
+        <PostContainer>
+          {/* 제목 헤더 */}
+          <PostTitleContainer>
+            <PostTitleTop>
+              <PostTitleTopLeft>
+                <PostCategory>App / Android Studio</PostCategory>
+                <PostTitle>
+                  <PostTitleText>[Android Studio] Paging</PostTitleText>
+                  <PostTitleLine/>
+                </PostTitle>
+              </PostTitleTopLeft>
+                
+              <PostTitleTopRight>
+                <PostShareBtn src={shareIcon}/>
+              </PostTitleTopRight>
+            </PostTitleTop>
+            
+            <PostTitleBottom>
+
+            </PostTitleBottom>
+
+          </PostTitleContainer>
+          <TitleBottomLine/>
+
+          {/* 본문 내용(마크다운) */}
+
+          {/* 해시태그, 좋아요, 댓글 */}
+
+          {/* 이전글, 다음글 이동 버튼 */}
+
+        </PostContainer>
+
+        {/* 카테고리 메뉴바 */}
+        <CategoryContainer>
+          카테고리
+        </CategoryContainer>
+      </Container>
+
+      <Footer />
+    </ContentFooter>
+  );
+}
+
+const ContentFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  border-radius: 10px;
+  border: 2px solid ${palette.mainColor};
+  margin-top: 20px;
+  padding: 92px 43px 38px 46px;
+  font-family: GmarketSansTTFMedium;
+`;
+
+const PostContainer = styled.div`
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  padding-right: 42px;
+`;
+const PostTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: ${palette.white};
+`;
+const TitleBottomLine = styled.div`
+  width: 100%;
+  height: 3.5px;
+  background: ${palette.lightGreen};
+`;
+const PostTitleTop = styled.div`
+  display: flex;
+  margin-bottom: 32px;
+`;
+const PostTitleTopLeft = styled.div`
+  flex: 5;
+  display: flex;
+  flex-direction: column;
+`;
+const PostTitleTopRight = styled.div`
+  flex: 1;
+  display: flex;
+`;
+const PostTitleBottom = styled.div`
+  display: flex;
+`;
+const PostCategory = styled.div`
+  color: ${palette.gray65};
+  font-size: 15px;
+  margin: 19px 0px 20px 22px;
+`;
+const PostTitle = styled.div`
+  display: flex;
+  position: relative;
+  width: fit-content;
+  margin-left: 22px;
+`;
+const PostTitleText = styled.div`
+  color: ${palette.gray3C};
+  font-family: GmarketSansTTFMedium;
+  font-size: 28px;
+  z-index: 100;
+`;
+const PostTitleLine = styled.div`
+  width: 100%;
+  height: 15px;
+  background: ${palette.highlightGreen};
+  position: absolute;
+  bottom: -7px;
+  margin-right: auto;
+`;
+const PostShareBtn = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 25px 25px 0px auto;
+`;
+const PostDate = styled.div`
+  color: ${palette.gray82};
+  font-size: 13px;
+`;
+const PostViewCount = styled.div`
+  font-size: 13px;
+`;
+
+const CategoryContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+
+export default PostDetail;
